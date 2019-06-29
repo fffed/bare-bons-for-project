3c934c9bc65502ea5afe7841a8bd44a801ffc64b
fffed <fffed@example.com>
2019-06-29 21:34:21 +0300

    implement Modular Routing with Express Router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
 import 'dotenv/config';
-import uuidv4 from 'uuid/v4';
 
+import routes from './routes';
 import models from './models';
 
 
@@ -22,64 +22,13 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
 
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
-});
-
-app.get('/session', (req, res) => {
-    return res.send(req.context.models.users[req.context.me.id]);
-});
-
-app.get('/users', (req, res) => {
-    return res.send(Object.values(req.context.models.users));
-});
-
-app.get('/users/:userId', (req, res) => {
-    return res.send(req.context.models.users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(req.context.models.messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
-
-app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-        id,
-        text: req.body.text,
-        userId: req.context.me.id,
-    };
-
-    req.context.models.messages[id] = message;
-
-    return res.send(message);
-});
-
-
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-});
-
-
-app.delete('/messages/:messageId', (req, res) => {
-    const {
-        [req.params.messageId]: message,
-        ...otherMessages
-    } = req.context.models.messages;
-
-    req.context.models.messages = otherMessages;
-
-    return res.send(message);
 });
 
 

65414b10a960eee1e8aff08238497be0ee147885
fffed <fffed@example.com>
2019-06-29 18:40:44 +0300

    add Express.js as a server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import express from 'express';
 import 'dotenv/config';
+
+
+const app = express();
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
 
 
 console.log('Hello Node.js project');

cf9c9970444384998e7fcc0185a2c5518de4961d
fffed <fffed@example.com>
2019-06-29 19:48:35 +0300

    mimic a database for REST

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import cors from 'cors';
 import express from 'express';
 import 'dotenv/config';
 
+import { users, messages } from './tmpData';
+
 
 const app = express();
 
@@ -9,22 +11,42 @@ app.use(cors()); // all routes are extended with CORS HTTP headers. By default a
 
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
-  });
-  
-  app.post('/', (req, res) => {
+});
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+});
+
+
+
+app.post('/', (req, res) => {
     return res.send('Received a POST HTTP method');
-  });
-  
-  app.put('/', (req, res) => {
+});
+
+
+app.put('/', (req, res) => {
     return res.send('Received a PUT HTTP method');
-  });
-  
-  app.delete('/', (req, res) => {
+});
+
+
+app.delete('/', (req, res) => {
     return res.send('Received a DELETE HTTP method');
-  });
-  
+});
+
+
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+    console.log(`Example app listening on port ${process.env.PORT}!`),
 );
 
 

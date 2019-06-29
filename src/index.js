8b7e3ff41880e25efcdf55f6c46e089271bcb43c
fffed <fffed@example.com>
2019-06-29 19:34:14 +0300

    add methods for REST Api

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ const app = express();
 app.use(cors()); // all routes are extended with CORS HTTP headers. By default all routes are accessible for all domains now.
 
 app.get('/', (req, res) => {
-    res.send('Hello The Root!');
+    return res.send('Received a GET HTTP method');
   });
   
-
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+  
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+  
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  });
+  
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
